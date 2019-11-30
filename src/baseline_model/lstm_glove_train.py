0b560fb12db56c503f3fe19850e5cafaacce8d7e
stellasylee <leeseoye@grinnell.edu>
2019-11-30 17:51:19 -0600

    lstm training

--- src/baseline_model/lstm_glove_train.py
+++ src/baseline_model/lstm_glove_train.py
@@ -1,19 +1,22 @@
 import sys
 sys.path.append('../')
-import preprocessing.preprocessing as Preprocess
+from preprocessing.preprocessing import transform
 import os
 import pickle
 import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, CSVLogger, \
+     TensorBoard, RemoteMonitor, ModelCheckpoint
 from tensorflow.keras.layers import Dense, Dropout, RepeatVector, Embedding, LSTM
 from tensorflow.keras.layers import add
 from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.preprocessing.text import Tokenizer as T
+from tensorflow.keras.utils import to_categorical
 import numpy as np
+import time
 
 data_path = '/home/stellasylee/Documents/CSC395/question-answering/script/data/merged'
 
 BATCH_SIZE = 32 # batch size for training
-NBR_EPOCHS = 50
+NBR_EPOCHS = 100
 MAX_CONTEXT = 30
 MAX_QUESTION = 10
 
@@ -23,8 +26,7 @@ MAX_QUESTION = 10
 
 # Get tokenizer
 with open(os.path.join(data_path, 'special/tokenizer.p'), 'rb') as f:
-    tokenizer = pickle.loads(f)
-    # TODO: TypeError: a bytes-like object is required, not '_io.BufferedReader'
+    tokenizer = pickle.load(f)
 vocab_size = len(tokenizer.word_index) + 1
 
 # Get embedding matrix
@@ -39,17 +41,84 @@ with open(os.path.join(data_path, 'Question_Train.txt'), 'r') as f:
 with open(os.path.join(data_path, 'Answer_Train.txt'), 'r') as f:
     answer = f.read().strip().split('\n')
 
-context = Preprocess.transform(context, max_len=MAX_CONTEXT, tokenizer=tokenizer)
-question = Preprocess.transform(question, max_len=MAX_QUESTION, tokenizer=tokenizer)
-answer = Preprocess.transform(answer, max_len=1, tokenizer=tokenizer)
+context = transform(context, max_len=MAX_CONTEXT, tokenizer=tokenizer)
+question = transform(question, max_len=MAX_QUESTION, tokenizer=tokenizer)
+answer = transform(answer, max_len=1, tokenizer=tokenizer)
+
+# generate one hot encode
+encoded_answer = to_categorical(tf.squeeze(answer, axis=1), num_classes=vocab_size)
 
 ###################################
 #       Model                     #
 ###################################
 context_model = Sequential()
-context_model.add(Embedding(vocab_size, output_dim=300, weights = [embedding_matrix]))
-#story_rnn.add(Embedding(vocab_size, EMBED_HIDDEN_SIZE, input_length=story_maxlen))
+context_model.add(Embedding(vocab_size, output_dim=300, weights = [embedding_matrix],
+                            input_length=MAX_CONTEXT, trainable=False))
 context_model.add(Dropout(0.3))
-
 # summarize the model
 print(context_model.summary())
+
+# generate embeddings for question and make adaptable to story
+question_model = Sequential()
+question_model.add(Embedding(vocab_size, output_dim=300, weights = [embedding_matrix],
+                             input_length=MAX_QUESTION, trainable=False))
+question_model.add(Dropout(0.3))
+question_model.add(LSTM(300, return_sequences=False))
+question_model.add(RepeatVector(MAX_CONTEXT))
+
+print(question_model.summary())
+
+# merge the two
+merged_model = add([context_model.output, question_model.output])
+
+model_combined = Sequential()
+model_combined.add(LSTM(300, return_sequences=False))
+model_combined.add(Dropout(0.3))
+model_combined.add(Dense(vocab_size, activation="softmax"))
+
+# combine models
+final_model = Model([context_model.input, question_model.input], model_combined(merged_model))
+print(final_model.summary())
+final_model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
+print("Training...")
+
+path = os.getcwd()
+checkpoint_dir = os.path.join(path, 'checkpoints')
+log_dir = os.path.join(path, 'logs')
+
+if not os.path.isdir(checkpoint_dir):
+    os.mkdir(checkpoint_dir)
+if not os.path.isdir(log_dir):
+    os.mkdir(log_dir)
+
+checkpoints = ModelCheckpoint(filepath='weights.epoch{epoch:02d}-val_loss{val_loss:.2f}.hdf5',
+                              monitor='val_loss',
+                              verbose=1,
+                              save_best_only=False,
+                              save_weights_only=False,
+                              mode='min',
+                              period=1)
+early_stopping = EarlyStopping(monitor='val_loss',
+                               min_delta=0.0,
+                               patience=15,
+                               verbose=1)
+csv_logger = CSVLogger(filename='training_log.csv',
+                       separator=',',
+                       append=True)
+remote = RemoteMonitor()
+tensorboard = TensorBoard(log_dir="{}/{}".format(log_dir, time.time()),
+                          histogram_freq=0,
+                          batch_size=BATCH_SIZE,
+                          write_graph=True,
+                          write_grads=True,
+                          write_images=True,
+                          update_freq='batch')
+reduce_lr = ReduceLROnPlateau(monitor='val_loss',
+                              factor=0.2,
+                              patience=15,
+                              min_lr=0.001)
+callbacks = [checkpoints, early_stopping, csv_logger, tensorboard, reduce_lr, remote]
+
+final_model.fit(x = [context, question],  y=encoded_answer,
+          batch_size=BATCH_SIZE, epochs=NBR_EPOCHS, validation_split=0.05, callbacks= callbacks)
